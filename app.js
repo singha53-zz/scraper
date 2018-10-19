bc5b51182626252f69c3b8ac2e4a080e8611f4ce
Amrit Singh <Amrit.Singh@hli.ubc.ca>
2018-10-19 17:59:28 -0400

    add morgan logger

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var exphbs  = require('express-handlebars');
+var logger = require("morgan");
 
+var PORT = process.env.PORT || 3000;
+
+// Initialize Express
 var app = express();
 
-// Middleware
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
 app.use(express.static("public"));
 
 // Handlebars
@@ -22,7 +29,7 @@ app.set("view engine", "handlebars");
 // Routes
 require('./routes/apiRoutes')(app)
 
-var PORT = process.env.PORT || 3000;
+// Start the server
 app.listen(PORT, function() {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",

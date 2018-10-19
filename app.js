bc0b86eabc1102e98d20322487db7dea74368c06
Amrit Singh <Amrit.Singh@hli.ubc.ca>
2018-10-19 15:50:58 -0400

    add site webpages, css and js

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var exphbs  = require('express-handlebars');
+
+var app = express();
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+// Handlebars
+app.set('views', './views')
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
+app.set("view engine", "handlebars");
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+var PORT = process.env.PORT || 3000;
+app.listen(PORT, function() {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });

fd3cac720b8c15c456bcc484561e4ee6f1f68f0f
Amrit Singh <Amrit.Singh@hli.ubc.ca>
2018-10-23 17:12:34 -0400

    update scraper api function

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -14,10 +14,6 @@ module.exports = function (app) {
     res.render('home');
   });
 
-  app.get("/api/headlines?saved=false", function(req, res){
-
-  });
-
   app.get("/api/headlines/:id", function(req, res){
 
   });
@@ -26,36 +22,39 @@ module.exports = function (app) {
   app.get("/api/fetch", function(req, res){
 // A GET route for scraping the nytimes website
   // First, we grab the body of the html with axios
-  axios.get("https://www.nytimes.com//").then(function(response) {
+  axios.get("https://www.nytimes.com/").then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     const $ = cheerio.load(response.data);
 
     // Now, we grab every h2 within an article tag, and do the following:
-    $("article a").each(function(i, element) {
+    $("article").each(function(i, element) {
 
-      // console.log($(this).children("a"))
       // Save an empty result object
       var result = {};
+      result.headline = $(element).find("h2").text().trim();
+      result.url = 'https://www.nytimes.com' + $(element).find("a").attr("href");
+      result.summary = $(element).find("p").text().trim();
 
-      // Add the text and href of every link, and save them as properties of the result object
-      result.headline = $(this).text()
-      result.url = 'https://www.nytimes.com/' + $(this).attr("href")
+      if (result.headline !== '' && result.summary !== ''){
+			db.Article.find({headline: result.headline}, function(err, data) {
+				if (data.length === 0) {
+          result.saved = false;
+					db.Article.create(result, function(err, data) {
+						if (err) throw err;
+					});
+				}
+			});
+      }
 
-      // Create a new Article using the `result` object built from scraping
-      db.Article.create(result)
-        .then(function(dbArticle) {
-          // View the added result in the console
-          console.log(dbArticle);
-        })
-        .catch(function(err) {
-          // If an error occurred, send it to the client
-          return res.json(err);
-        });
-    });
+      });
 
     // If we were able to successfully scrape and save an Article, send a message to the client
     res.send("Scrape Complete");
   });
+  });
+
+  app.get("/api/headlines?saved=false", function(req, res){
+
   });
 
   app.get("api/clear", function(req, res){

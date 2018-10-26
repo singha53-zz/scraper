467753c2363103f8696ad188735b852178dced14
Amrit Singh <Amrit.Singh@hli.ubc.ca>
2018-10-26 00:17:06 -0400

    update index.js

--- public/assets/javascript/index.js
+++ public/assets/javascript/index.js
@@ -123,9 +123,9 @@ $(document).ready(function() {
       // and let the user know how many unique articles we were able to save
       // initPage();
       console.log(data)
-      data.message = "Scrape completed!"
-      bootbox.alert($("<h3 class='text-center m-top-80'>").text(data.message));
-      location.reload();
+      // data.message = "Scrape completed!"
+      // bootbox.alert($("<h3 class='text-center m-top-80'>").text(data.message));
+      window.location.href = "/";
     });
   }
 
